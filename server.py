3fd2cabc8fd888a60b5a2024cd6649d712f0461c
Nico Suarez <nicodoggie@gmail.com>
2025-06-29 08:19:38 -0400

    refactor: pylint issues

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Server for Emotion analysis"""
+
 from operator import itemgetter
 from flask import Flask, request, render_template
 from EmotionDetection import emotion_detector
@@ -6,12 +8,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Web App entrypoint"""
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def get_emotion_detector():
+    """Route handler for analyzing emotion strings"""
     text_to_analyze = request.args.get('textToAnalyze', '')
-    
+
     result = emotion_detector(text_to_analyze)
     anger, disgust, fear, sadness, dominant_emotion = itemgetter(
         'anger', 'disgust', 'fear', 'sadness', 'dominant_emotion'
@@ -20,8 +24,8 @@ def get_emotion_detector():
     if dominant_emotion is None:
         return "invalit text! Please try again!"
 
-    return f"For the given statement, " \
-        + f"the system response is "\
+    return "For the given statement, " \
+        + "the system response is "\
         + f"'anger': {anger}, 'disgust': {disgust}. "\
         + f"'fear': {fear}, 'sadness': {sadness}. " \
         + f"The dominant emotion is {dominant_emotion}/"
