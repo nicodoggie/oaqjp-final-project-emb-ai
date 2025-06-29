57b9d6a33fd78e8f136a7c0b9e7327189cba1bba
Nico Suarez <nicodoggie@gmail.com>
2025-06-29 07:23:09 -0400

    feat: formatted emotion dictionary output
     - fetch only the first emotion
     - add the dominant emotion to the return structure

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from operator import itemgetter
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -9,7 +11,23 @@ def emotion_detector(text_to_analyze):
 
     if response:
         if response.status_code == 200:
-            return response.text
+            text_response = response.text
+            json_response = json.loads(text_response)
+            # let's extract the first prediction only
+            prediction = json_response["emotionPredictions"][0]["emotion"]
+            emotion_obj = {}
+            dominant_emotion = ''
+            dominant_score = 0
+            for emotion, score in prediction.items():
+                print(emotion, score)
+                score = float(score)
+                emotion_obj[emotion] = score
+                if score >= dominant_score:
+                    dominant_emotion = emotion
+                    dominant_score = score
+
+            emotion_obj['dominant_emotion'] = dominant_emotion
+            return emotion_obj
         else:
             return f"received error code {response.status_code} with text {response.text}"
     else:
