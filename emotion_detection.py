353ed1cee2f5f989ae48aede86f572f78bc8d56d
Nico Suarez <nicodoggie@gmail.com>
2025-06-29 07:01:46 -0400

    feat: implemented emotion detector function
    retrieve info from skills network watson x instance

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json=data, headers=headers)
+
+    if response:
+        if response.status_code == 200:
+            return response.text
+        else:
+            return f"received error code {response.status_code} with text {response.text}"
+    else:
+        raise Exception(f'Failed to retrieve emotion data')
